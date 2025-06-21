c9953e807fb0873972795b3437d2f6ab05bdf714
Ann Huang <ann82925902@gmail.com>
2025-06-21 16:23:03 +0800

    update

--- lesson13/main.py
+++ lesson13/main.py
@@ -1,8 +1,12 @@
 import streamlit as st
+import pandas as pd
 
 def main():
     st.title("我的第一個Streamlit App")
     st.write("歡迎來到我的應用程式！")
+    
+    df = pd.read_csv("taiwan.csv")
+    st.write(df)
 
 if __name__ == "__main__":
     main()
